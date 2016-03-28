59830296f101b89e559be1ef778bc88dc64e435d
marcus <WittigMarcus@gmail.com>
2016-03-28 18:15:02 +0200

    Return top-level request object for compatibility with http API, provide support for "no-auth" if server does no request client authentication, handle timeout on chained request, forward error and time events of  chained request to top-level request,

--- http-digest-client.js
+++ http-digest-client.js
@@ -25,9 +25,10 @@ var HTTPDigest = function () {
   //
   HTTPDigest.prototype.request = function (options, callback) {
     var self = this;
-    http.request(options, function (res) {
-      self._handleResponse(options, res, callback);
-    }).end();
+    var req = http.request(options, function (res) {
+      self._handleResponse(options, req, res, callback);
+    });
+    return req;
   };
 
   //
@@ -35,61 +36,74 @@ var HTTPDigest = function () {
   //
   // Parse authentication headers and set response.
   //
-  HTTPDigest.prototype._handleResponse = function handleResponse(options, res, callback) {
-    var challenge = this._parseChallenge(res.headers['www-authenticate']);
-    var ha1 = crypto.createHash('md5');
-    ha1.update([this.username, challenge.realm, this.password].join(':'));
-    var ha2 = crypto.createHash('md5');
-    ha2.update([options.method, options.path].join(':'));
+  HTTPDigest.prototype._handleResponse = function handleResponse(options, clientRequest, res, callback) {
+    if (res.headers.hasOwnProperty('www-authenticate')) {
+      var challenge = this._parseChallenge(res.headers['www-authenticate']);
+      var ha1 = crypto.createHash('md5');
+      ha1.update([this.username, challenge.realm, this.password].join(':'));
+      var ha2 = crypto.createHash('md5');
+      ha2.update([options.method, options.path].join(':'));
 
-    // Generate cnonce
-    var cnonce = false;
-    var nc = false;
-    if (typeof challenge.qop === 'string') {
-      var cnonceHash = crypto.createHash('md5');
-      cnonceHash.update(Math.random().toString(36));
-      cnonce = cnonceHash.digest('hex').substr(0, 8);
-      nc = this.updateNC();
+      // Generate cnonce
+      var cnonce = false;
+      var nc = false;
+      if (typeof challenge.qop === 'string') {
+        var cnonceHash = crypto.createHash('md5');
+        cnonceHash.update(Math.random().toString(36));
+        cnonce = cnonceHash.digest('hex').substr(0, 8);
+        nc = this.updateNC();
+      }
+
+      // Generate response hash
+      var response = crypto.createHash('md5');
+      var responseParams = [
+        ha1.digest('hex'),
+        challenge.nonce
+      ];
+
+      if (cnonce) {
+        responseParams.push(nc);
+        responseParams.push(cnonce);
+      }
+
+      responseParams.push(challenge.qop);
+      responseParams.push(ha2.digest('hex'));
+      response.update(responseParams.join(':'));
+
+      // Setup response parameters
+      var authParams = {
+        username: this.username,
+        realm: challenge.realm,
+        nonce: challenge.nonce,
+        uri: options.path,
+        qop: challenge.qop,
+        response: response.digest('hex'),
+        opaque: challenge.opaque
+      };
+      if (cnonce) {
+        authParams.nc = nc;
+        authParams.cnonce = cnonce;
+      }
+
+      var headers = options.headers || {};
+      headers.Authorization = this._compileParams(authParams);
+      options.headers = headers;
+
+      var req = http.request(options, function (res) {
+        callback(res);
+      });
+      if (options.hasOwnProperty('timeout')) {
+        req.setTimeout(options.timeout);
+      }
+      req.on('error', function forwardError (err) {
+        clientRequest.emit('error', err);
+      });
+      req.on('timeout', function forwardTimeout () {
+        clientRequest.emit('timeout');
+        req.abort();
+      });
+      req.end();
     }
-
-    // Generate response hash
-    var response = crypto.createHash('md5');
-    var responseParams = [
-      ha1.digest('hex'),
-      challenge.nonce
-    ];
-
-    if (cnonce) {
-      responseParams.push(nc);
-      responseParams.push(cnonce);
-    }
-
-    responseParams.push(challenge.qop);
-    responseParams.push(ha2.digest('hex'));
-    response.update(responseParams.join(':'));
-
-    // Setup response parameters
-    var authParams = {
-      username: this.username,
-      realm: challenge.realm,
-      nonce: challenge.nonce,
-      uri: options.path,
-      qop: challenge.qop,
-      response: response.digest('hex'),
-      opaque: challenge.opaque
-    };
-    if (cnonce) {
-      authParams.nc = nc;
-      authParams.cnonce = cnonce;
-    }
-
-    var headers = options.headers || {};
-    headers.Authorization = this._compileParams(authParams);
-    options.headers = headers;
-
-    http.request(options, function (res) {
-      callback(res);
-    }).end();
   };
 
   //
